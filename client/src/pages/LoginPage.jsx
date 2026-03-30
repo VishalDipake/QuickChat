1b54fe164ddd572d7cfab9ef1d17b3e513282e98
VishalDipake <vishaldipake072@gmail.com>
2026-03-30 15:14:49 +0530

    Front End Completed

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,159 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const LoginPage = () => {
+
+  const [currState, setCurrState] = useState("Sign up")
+  const [fullName, setFullName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [bio, setBio] = useState("")
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false)
+
+  const onSubmitHandler =(event)=>{
+      event.preventDefault();
+
+      if(currState === "Sign up" && !isDataSubmitted){
+        setIsDataSubmitted(true)
+        return;
+      }
+  }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+
+    // // Step 1 → go to bio screen
+    // if (currState === "Sign up" && !isDataSubmitted) {
+    //   if (!fullName || !email || !password) {
+    //     alert("Fill all fields first")
+    //     return
+    //   }
+    //   setIsDataSubmitted(true)
+    //   return
+    // }
+
+  //   // Final submit
+  //   console.log({
+  //     fullName,
+  //     email,
+  //     password,
+  //     bio
+  //   })
+
+  //   alert("Form Submitted 🚀")
+  // }
+
+  // const toggleState = () => {
+  //   setCurrState(currState === "Sign up" ? "Login" : "Sign up")
+  //   setIsDataSubmitted(false)
+  // }
+
   return (
-    <div>
-      <h1>login page
+
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+
+      {/* -------- left -------- */}
+      <img
+        src={assets.logo_big}
+        alt=""
+        className='w-[min(30vw,250px)]'
+      />
+
+      {/* -------- right -------- */}
+      <form
+        onSubmit={onSubmitHandler}
+        className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'
+      >
+
+        <h2 className='font-medium text-2xl flex justify-between items-center'>
+          {currState}
+          {isDataSubmitted &&  <img
+            onClick={()=> setIsDataSubmitted(false)}
+            src={assets.arrow_icon}
+            alt=""
+            className='w-5 cursor-pointer'
+          />}
         
-      </h1>
+        </h2>
+
+        {/* -------- Full Name -------- */}
+        {currState === "Sign up" && !isDataSubmitted && (
+          <input
+            onChange={(e) => setFullName(e.target.value)}
+            value={fullName}
+            type="text"
+            placeholder='Full Name'
+            required
+            className='p-2 border border-gray-500 rounded-md focus:outline-none'
+          />
+        )}
+
+        {/* -------- Email + Password -------- */}
+        {!isDataSubmitted && (
+          <>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              placeholder='Email Address'
+              required
+              className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            />
+
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              placeholder='Password'
+              required
+              className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            />
+          </>
+        )}
+
+        {/* -------- Bio Step -------- */}
+        {currState === "Sign up" && isDataSubmitted && (
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            rows={4}
+            placeholder='Provide a short bio...'
+            required
+            className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'
+          />
+        )}
+
+        {/* -------- Button -------- */}
+        <button
+          type='submit'
+          className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'
+        >
+          {currState === "Sign up"
+            ? (isDataSubmitted ? "Submit" : "Create Account")
+            : "Login"}
+        </button>
+
+        {/* -------- Terms -------- */}
+        <div className='flex items-center gap-2 text-sm text-gray-400'>
+          <input type="checkbox" required />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+
+        <div className='flex flex-col gap-2'>
+          {currState === "Sign up" ? (
+            <p className='text-sm text-gray-600'>Already have an account? <span onClick={()=>{
+              setCurrState('Login'); setIsDataSubmitted(false)
+            }}
+            className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
+          ): (
+            <p className='text-sm text-gray-600'>Create an account <span onClick={()=>{
+              setCurrState("Sign up")
+            }}
+            className='font-medium text-violet-500 cursor-pointer'>Click here gandu</span></p>
+          )}
+        </div>
+
+      </form>
     </div>
   )
 }
