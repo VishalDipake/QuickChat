960695d043155aba1728efe63981c1b7edc6628f
VishalDipake <vishaldipake072@gmail.com>
2026-04-01 19:08:33 +0530

    error resolved

--- server/server.js
+++ server/server.js
@@ -55,6 +55,10 @@ app.use("/api/messages", messageRouter)
 await connectDB();
 
 
-
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log("Server is running on PORT:" + PORT));
+}
+
+//Exporting server for testing purposes
+export default server;
