6336f9a553d7f7f5fccfbb1050fb7dc4514268bc
VishalDipake <vishaldipake072@gmail.com>
2026-04-02 10:37:34 +0530

    r

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
-import RightSideBar from '../components/RightSideBar'
+import RightSideBar from '../components/RightSidebar'
 import { useContext } from 'react'
 import { ChatContext } from '../../context/ChatContext'
 
